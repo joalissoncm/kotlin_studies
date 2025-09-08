8e0b025baa0789f5ff029e7234d1e8881dacd164
Maymisson Magno Goes <maymisson.goes+cielosa@cielo.com.br>
2025-09-08 13:18:37 -0300

    refactor groovy

---
+++ app/src/androidTest/java/com/example/helloworld/ExampleInstrumentedTest.kt
@@ -0,0 +1,18 @@
+package com.example.helloworld
+
+import android.content.Context
+import androidx.test.core.app.ApplicationProvider
+import androidx.test.ext.junit.runners.AndroidJUnit4
+import org.junit.Assert.assertEquals
+import org.junit.Test
+import org.junit.runner.RunWith
+
+@RunWith(AndroidJUnit4::class)
+class ExampleInstrumentedTest {
+    @Test
+    fun useAppContext() {
+        // Context of the app under test.
+        val appContext = ApplicationProvider.getApplicationContext<Context>()
+        assertEquals("com.example.helloworld", appContext.packageName)
+    }
+}
