4a987d30843db3cc9517ed0e9f11a0b694d0c5cf
Maymisson Magno Goes <maymisson.goes+cielosa@cielo.com.br>
2025-09-08 09:19:39 -0300

    recoding

--- app/src/androidTest/java/in/myinnos/kotlinexample/ExampleInstrumentedTest.java
+++ app/src/androidTest/java/in/myinnos/kotlinexample/ExampleInstrumentedTest.java
@@ -1,26 +1,17 @@
-package in.myinnos.kotlinexample;
+package in.myinnos.kotlinexample
 
-import android.content.Context;
-import android.support.test.InstrumentationRegistry;
-import android.support.test.runner.AndroidJUnit4;
+import androidx.test.platform.app.InstrumentationRegistry
+import androidx.test.ext.junit.runners.AndroidJUnit4
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.junit.Assert.*
 
-import org.junit.Test;
-import org.junit.runner.RunWith;
-
-import static org.junit.Assert.*;
-
-/**
- * Instrumentation test, which will execute on an Android device.
- *
- * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
- */
-@RunWith(AndroidJUnit4.class)
-public class ExampleInstrumentedTest {
+@RunWith(AndroidJUnit4::class)
+class ExampleInstrumentedTest {
     @Test
-    public void useAppContext() throws Exception {
+    fun useAppContext() {
         // Context of the app under test.
-        Context appContext = InstrumentationRegistry.getTargetContext();
-
-        assertEquals("in.myinnos.kotlinexample", appContext.getPackageName());
+        val appContext = InstrumentationRegistry.getInstrumentation().targetContext
+        assertEquals("in.myinnos.kotlinexample", appContext.packageName)
     }
 }
--- app/src/main/java/in/myinnos/kotlinexample/MainActivity.kt
+++ app/src/main/java/in/myinnos/kotlinexample/MainActivity.kt
@@ -1,176 +1,22 @@
-package `in`.myinnos.kotlinexample
+package in.myinnos.kotlinexample
 
-import android.content.Context
-import android.net.Uri
 import android.os.Bundle
-import android.support.customtabs.CustomTabsIntent
-import android.support.v4.content.ContextCompat
-import android.support.v7.app.AppCompatActivity
-import android.view.Menu
-import android.view.MenuItem
-import android.view.inputmethod.InputMethodManager
-import android.widget.EditText
-import kotlinx.android.synthetic.main.activity_main.*
-import org.jetbrains.anko.*
-
+import androidx.activity.ComponentActivity
+import androidx.activity.viewModels
+import androidx.appcompat.app.AppCompatActivity
+import in.myinnos.kotlinexample.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
-
-    val HEADER_TEXT = "You can try awesome example!"
-    val EDIT_TEXT_NAME_HINT = "enter name"
-    val EDIT_TEXT_NUMBER_HINT = "enter number"
-    val EMAIL_ID = "contact@myinnos.in"
-    val GIT_HUB_URL = "https://github.com/myinnos/Kotlin-Example"
-    val GIT_HUB_WEB_URL = "https://myinnos.github.io/Kotlin-Example/";
-    val PLAY_STORE_URL = "market://details?id=" + BuildConfig.APPLICATION_ID
+    private lateinit var binding: ActivityMainBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
 
-        // setting header text
-        tvHeader.text = HEADER_TEXT
-        // setting hint for edit text
-        etName.hint = EDIT_TEXT_NAME_HINT
-        etMobile.hint = EDIT_TEXT_NUMBER_HINT
-
-        // setting drawable image to image view
-        imageView.image = resources.getDrawable(R.mipmap.ic_launcher)
-
-        // onclick event for image view to restart activity (Intent function)
-        imageView.onClick {
-            startActivity<SplashScreenActivity>()
-            finish()
-            toast("Activity restarted!")
+        binding.tvHeader.text = "Hello World!"
+        binding.btDone.setOnClickListener {
+            binding.tvHeader.text = "Olá, mundo!"
         }
-
-        // onclick event for button
-        btDone.onClick {
-            hideKeyboard()
-            if (!checkTextLength(etName) || !checkTextLength(etMobile))
-                toast("Fields cannot be empty!")
-            else
-                onButtonClicks()
-        }
-
-        btGitHubLink.onClick {
-            // opining browser intent
-            browse(GIT_HUB_URL)
-        }
-
-        btTutorial.onClick {
-            // google custom tabs
-            val builder = CustomTabsIntent.Builder()
-            builder.setToolbarColor(ContextCompat.getColor(this, R.color.colorPrimary))
-            val customTabsIntent = builder.build()
-            customTabsIntent.launchUrl(this, Uri.parse(GIT_HUB_WEB_URL))
-        }
-    }
-
-    // function to get text from edit text
-    fun EditText.textValue(): String {
-        return text.toString()
-    }
-
-    // function to get text length of edit text
-    fun checkTextLength(editText: EditText): Boolean {
-
-        var length = editText.length()
-
-        return length > 0
-    }
-
-    // function to hide keyboard
-    fun hideKeyboard() {
-        try {
-            val imm = getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
-            imm.hideSoftInputFromWindow(currentFocus!!.windowToken, 0)
-        } catch (e: Exception) {
-            // TODO: handle exception
-        }
-
-    }
-
-    fun onButtonClicks() {
-        //using function
-        val phoneNumber = etMobile.textValue()
-        // direct access
-        val name = etName.text.toString()
-        // calling alert dialog
-        openAlertDialog(name, phoneNumber)
-    }
-
-    fun openAlertDialog(name: String, phoneNumber: String) {
-
-        val countries = listOf("Russia", "India", "USA", "Japan", "China")
-
-        selector("Where are you from?", countries) { i ->
-            //toast("So you're living in ${countries[i]}, right?")
-            alert("One more thing! You have entered this number " + phoneNumber, name + "! So you're living in ${countries[i]}, right?") {
-                customView {
-                    verticalLayout {
-                        positiveButton("AWESOME!") {
-                            longToast("Thank you!")
-                        }
-                    }
-                }
-
-            }.show()
-
-        }
-
-        // you can create awesome custom layout using blow alert dialog code
-        /*
-        alert {
-            customView {
-                verticalLayout {
-                    textView {
-                        text = name
-                        textColor = ContextCompat.getColor(context, R.color.colorPrimaryDark)
-                        textSize = 14F
-                    }
-                    textView {
-                        text = phoneNumber
-                        textColor = resources.getColor(R.color.colorPrimary)
-                        textSize = 12F
-                    }
-
-                    positiveButton("AWESOME!") { toast("Thank you!") }
-                }
-            }
-        }.show()*/
-
-    }
-
-    // Initializing menu options
-    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        menuInflater.inflate(R.menu.menu_main, menu)
-        return true
-    }
-
-    override fun onOptionsItemSelected(item: MenuItem?): Boolean {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        val id = item!!.getItemId()
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_share) {
-            // sharing intent
-            share(getString(R.string.share_text) + BuildConfig.APPLICATION_ID,
-                    getString(R.string.app_name))
-            return true
-        } else if (id == R.id.action_feedback) {
-            // email intent
-            email(EMAIL_ID, getString(R.string.app_name))
-            return true
-        } else if (id == R.id.action_rate) {
-            // opining browser intent
-            browse(PLAY_STORE_URL)
-            return true
-        }
-
-        return super.onOptionsItemSelected(item)
     }
 }
